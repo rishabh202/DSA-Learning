ce8a0889daf395508b96d40c26b8ff658ea9ca34
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-23 23:36:23 +0530

    solving some questions on trees

---
+++ example1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+// Find the height of a binary tree: 
+// Height is simply the largest path between root node and leaf node
+
+
+
+return 0;
+}
